e8e30b43eb0e5f9ebefdb282980748f87db18f3e
pstroh <philip.stroh@valador.com>
2015-06-23 17:14:23 -0400

    #16560 Develop the User Interface Including Responsive Design -  Created a primitive bootstrap site with limited functionality for testing purposes.

--- frontend/js/auto-complete.js
+++ frontend/js/auto-complete.js
@@ -1,34 +1,35 @@
-var MIN_LENGTH = 3;
+  $(function() {
+    function log( message ) {
+      $( "<div>" ).text( message ).prependTo( "#log" );
+      $( "#log" ).scrollTop( 0 );
+    }
 
-$( document ).ready(function() {
-	$("#keyword").keyup(function() {
-		var keyword = $("#keyword").val();
-		if (keyword.length >= MIN_LENGTH) {
-
-			$.get( "autocomplete/auto-complete.php", { keyword: keyword } )
-			.done(function( data ) {
-				$('#results').html('');
-				var results = jQuery.parseJSON(data);
-				$(results).each(function(key, value) {
-					$('#results').append('<div class="item">' + value + '</div>');
-				})
-
-			    $('.item').click(function() {
-			    	var text = $(this).html();
-			    	$('#keyword').val(text);
-			    })
-
-			});
-		} else {
-			$('#results').html('');
-		}
-	});
-
-    $("#keyword").blur(function(){
-    		$("#results").fadeOut(500);
-    	})
-        .focus(function() {		
-    	    $("#results").show();
-    	});
-
-});
+    var keyword = $("#keyword").val();
+    
+    $( "#keyword" ).autocomplete({
+      source: function( request, response ) {
+        $.ajax({
+          url: "autocomplete/auto-complete.php",
+          dataType: "json",
+          data: {
+            keyword: request.term
+          },
+          success: function( data ) {
+            response( data );
+          }
+        });
+      },
+      minLength: 3,
+      select: function( event, ui ) {
+        log( ui.item ?
+          "Selected: " + ui.item.label :
+          "Nothing selected, input was " + this.value);
+      },
+      open: function() {
+        $( this ).removeClass( "ui-corner-all" ).addClass( "ui-corner-top" );
+      },
+      close: function() {
+        $( this ).removeClass( "ui-corner-top" ).addClass( "ui-corner-all" );
+      }
+    });
+  });
