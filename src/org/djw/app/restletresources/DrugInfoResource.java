c25ad34e2b64985420490eb29bfcebc92691c05f
davwhite <davwhite@davwhite.com>
2015-06-26 19:48:49 -0400

    Updated DrugInfo service

--- src/org/djw/app/restletresources/DrugInfoResource.java
+++ src/org/djw/app/restletresources/DrugInfoResource.java
@@ -3,7 +3,6 @@ package org.djw.app.restletresources;
 
 import openfda.classes.DailyMedClient;
 import openfda.classes.OpenFDAClient;
-import openfda.classes.PillBoxClient;
 import openfda.classes.ServerAuth;
 
 import org.apache.log4j.Logger;
@@ -74,13 +73,7 @@ public class DrugInfoResource extends ServerResource {
 					}
 				}
 				
-				
-				ServiceURI = "/pillboxAPIService.php?key=CGCUZFWCM4&ingredient=aspirin&has_image=1";
-				PillBoxClient pillClient = new PillBoxClient();
-				String jsonPill = pillClient.getService(ServiceURI);
-logger.debug(jsonPill);
-				
-				
+								
 				Message = "";
 				Status = 0;
 				DrugInfo.put("images", images);
